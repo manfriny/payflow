e9ecb6ccc3b1b2b6a10839f54afe2fccb395b77f
Manfriny <manfriny@gmail.com>
2021-08-13 21:04:22 -0300

    20210813 finalizou aula 3

---
+++ lib/modules/home/home_controller.dart
@@ -0,0 +1,8 @@
+class HomeController {
+  var currentPage = 0;
+
+  //funcoes
+  void setPage(int index) {
+    currentPage = index;
+  }
+}
--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -12,6 +13,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
+
   @override
   Widget build(BuildContext context) {
     //pegar o tamanho da tela
@@ -62,7 +65,7 @@ class _LoginPageState extends State<LoginPage> {
                         const EdgeInsets.only(top: 40, left: 40, right: 40),
                     child: SocialLoginButton(
                       onTap: () {
-                        print("clicou");
+                        controller.googleSignIn(context);
                       },
                     ),
                   ),
